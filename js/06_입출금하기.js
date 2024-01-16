f54675ca5efeae95fe98451f121e5b222a50d8de
haechankim <haechan0202@naver.com>
2024-01-16 17:50:35 +0900

    20240116 수업내용

--- js/06_입출금하기.js
+++ js/06_입출금하기.js
@@ -1,22 +1,32 @@
+const amount = document.getElementById("amount"); //잔액 출력 span
+const output = document.getElementById("output"); // 금액 입력 input
 
-const balance = document.getElementById("balance");
-const amount = document.getElementById("amount");
+let balance = 10000; 
+const password = '1234';
 
 function deposit(){
-      let balance = 10000;
-    
+    // 입금 버튼 클릭 시
+    // input에 입력된 금액 만큼 잔액에 추가
 
+    if(amount.value.length == 0){
+        alert("금액을 입력해주세요");
+    }else{
+        balance += Number(amount.value);
+        output.innerText = balance;
+
+        amount.value ''; //input에 작성된 값 제거
+    }
 }
-
-
-
-
-
 
 
 function withdrawal(){
     
-    const password = prompt("비밀번호를 입력하세요");
+    if(amount.value.length ==){
+        alert("금액을 입력해주세요");
+
+    }else{
+
+        const password = prompt("비밀번호를 입력하세요");
 
     if(password == null){
         alert("취소");
@@ -28,4 +38,8 @@ function withdrawal(){
             alert("비밀번호가 일치하지 않습니다");
         }
     }
+
+    }
+
+    
 }
